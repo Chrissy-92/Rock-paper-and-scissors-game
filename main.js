a895d9d6cf6802a76af946bdcf1033b764327244
Cristina Pérez <cristinaperezhuerta@gmal.com>
2025-05-07 10:24:34 +0200

    Working on the comparison function, player function and machine function, done

--- main.js
+++ main.js
@@ -9,34 +9,42 @@ const machineScore = document.querySelector('.js_machine');
 // FUNCIONES
 
 const getOptionSelectedByUser = () => {
-    const option = gameSelect.value;
-    return option;
+    const player = gameSelect.value;
+    return player;
 };
+
+const gameSelectPlayer = () => {
+    const player = getOptionSelectedByUser();
+
+    if ( player === 'piedra' ) {
+    console.log('el jugador ha elegido piedra');
+    } else if ( player === 'papel') {
+    console.log('el jugador ha elegido papel');
+    } else if ( player === 'tijera') {
+    console.log('el jugador ha elegido tijera');
+    }
+}
 
 function getRandomNumber(max) { 
     return Math.ceil(Math.random() * max); 
-  }
+}
 
-  const gameSelectMachine = getRandomNumber
-
-
-  if ( getRandomNumber <= 3) {
+if ( getRandomNumber <= 3) {
     console.log ('piedra');
   } else if ( getRandomNumber >= 7) {
     console.log ('papel');
   } else {
     console.log ('tijera');
-  }
+}
 
-
-
+const gameSelectMachine = getRandomNumber;
 
 // FUNCIONES DE EVENTOS
 
 const handleClickGame = (event) => {
     event.preventDefault();
 
-    gameSelect();
+    gameSelectPlayer();
     gameSelectMachine();
 };
 
